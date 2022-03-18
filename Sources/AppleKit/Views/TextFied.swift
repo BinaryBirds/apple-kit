7bf2d066ffd89035c1f6d4d0b5ebdb26ff0cbfc5
Tibor Bödecs <mail.tib@gmail.com>
2022-03-18 13:35:24 +0100

    text field updates

--- Sources/AppleKit/Views/TextFied.swift
+++ Sources/AppleKit/Views/TextFied.swift
@@ -8,6 +8,15 @@
 #if canImport(UIKit)
 open class TextField: UITextField {
     
+    var editingChangeHandler: ((TextField) -> Void)?
+    var editingDidEndOnExitHandler: ((TextField) -> Void)?
+    var doneHandler: ((TextField) -> Void)?
+    
+    public var value: String { text ?? "" }
+    public var isEmpty: Bool { value.isEmpty }
+    
+    // MARK: - init
+    
     @available(*, unavailable)
     private override init(frame: CGRect) {
         super.init(frame: frame)
@@ -46,6 +55,41 @@ open class TextField: UITextField {
         leftViewMode = .always
         rightView = paddingView
         rightViewMode = .always
+        
+        self.addTarget(self, action: #selector(editingChangedAction(_:)), for: .editingChanged)
+        self.addTarget(self, action: #selector(editingDidEndOnExitAction(_:)), for: .editingDidEndOnExit)
+    }
+    
+    // MARK: - actions
+    
+    @objc private func editingChangedAction(_ sender: Any) {
+        self.editingChangeHandler?(self)
+    }
+
+    @objc private func editingDidEndOnExitAction(_ sender: Any) {
+        self.editingDidEndOnExitHandler?(self)
+    }
+
+    @objc private func doneAction(_ sender: Any) {
+        self.doneHandler?(self)
+    }
+
+    @discardableResult
+    public func onEditingChange(_ handler: @escaping ((TextField) -> Void)) -> Self {
+        self.editingChangeHandler = handler
+        return self
+    }
+    
+    @discardableResult
+    public func onEditingDidEndOnExit(_ handler: @escaping ((TextField) -> Void)) -> Self {
+        self.editingDidEndOnExitHandler = handler
+        return self
+    }
+
+    @discardableResult
+    public func onDone(_ handler: @escaping ((TextField) -> Void)) -> Self {
+        self.doneHandler = handler
+        return self
     }
 }
 
